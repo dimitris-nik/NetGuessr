e07fdede6424e59a44e03367c94f98d098855df9
Dimitris Nikolaidis <dim.nik.public@gmail.com>
2025-03-20 21:17:36 +0200

    Added animated dots <3

--- NetGuessr/src/App.jsx
+++ NetGuessr/src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-
 import Website from "./Website"
 
 const URL = "http://localhost:5000/getRandomWebsite";
--- NetGuessr/src/Website.jsx
+++ NetGuessr/src/Website.jsx
@@ -1,9 +1,10 @@
 import  { useEffect, useState } from 'react'
+import { Dot } from "react-animated-dots";
 
 export default function Website({url}) {
     return (
     <div className="iframe-container">
         {url.length > 0 && <iframe src={url} title="Website" />}
-        {url.length === 0 && <p>Loading...</p>}
+        {url.length === 0 && <p>Loading<Dot>.</Dot><Dot>.</Dot><Dot>.</Dot></p>}
     </div>)
 }
